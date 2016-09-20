50468ba5987bdefe1cbbcdf485d7058ca58c4c40
Ouzo_12 <ouzo12@gmail.com>
2016-09-21 00:48:27 +0200

    perfdata for senderscore.sh

--- server-check/senderscore/senderscore.sh
+++ server-check/senderscore/senderscore.sh
@@ -5,7 +5,7 @@ poor="60"
 
 if [ "$1" = "" ]; then
 	echo "Usage: $0 <ip>"
-	echo "Neutral is $neutral - Poor is $poor - Everything better that neutral is GOOD!"
+	echo "Neutral is $neutral - Poor is $poor - Everything better than neutral is GOOD!"
 	exit 2
 fi
 
@@ -18,14 +18,14 @@ if [ "$senderscoretest" = "" ]; then
 	exit 0
  else
 	if [ `echo "$senderscore > $neutral" | bc -l` = 1 ]; then
-		echo "SenderScore: $senderscore - Status: GOOD"
+		echo "SenderScore: $senderscore - Status: GOOD|senderscore=$senderscore;$neutral;$poor;0;"
 		exit 0
 	fi
 	if [ `echo "$senderscore > $poor" | bc -l` = 1 ]; then
-		echo "SenderScore: $senderscore - Status: NEUTRAL - Could be better."
+		echo "SenderScore: $senderscore - Status: NEUTRAL - Could be better.|senderscore=$senderscore;$neutral;$poor;0;"
 		exit 1
 	else
-		echo "SenderScore: $senderscore - Status: POOR - Info: https://www.senderscore.org/lookup.php?lookup=$1"
+		echo "SenderScore: $senderscore - Status: POOR - Info: https://www.senderscore.org/lookup.php?lookup=$1|senderscore=$senderscore;$neutral;$poor;0;"
 		exit 2
 	fi
 fi
